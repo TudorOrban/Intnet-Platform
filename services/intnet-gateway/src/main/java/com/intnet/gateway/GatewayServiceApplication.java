fca06d2266ce0be54e52bbf2229f75f61292a1be
TudorOrban <tudororban2@gmail.com>
2025-02-14 00:04:45 +0200

    feat(gateway): Flesh out API Gateway configuration

--- services/intnet-gateway/src/main/java/com/intnet/gateway/GatewayServiceApplication.java
+++ services/intnet-gateway/src/main/java/com/intnet/gateway/GatewayServiceApplication.java
@@ -2,8 +2,18 @@ package com.intnet.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		BatchAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,})
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
---
+++ services/intnet-gateway/src/main/java/com/intnet/gateway/core/config/GatewayRouteConfig.java
@@ -0,0 +1,22 @@
+package com.intnet.gateway.core.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GatewayRouteConfig {
+
+    private static final String TENANT_SERVICE = "http://intnet-tenant:80";
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Tenant
+                .route("test-route", r -> r.path("/api/v1/test/**")
+                        .uri(TENANT_SERVICE))
+                .build();
+    }
+}
