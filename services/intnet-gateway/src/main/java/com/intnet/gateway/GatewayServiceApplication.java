26f6279ba3c8dc9597fc554527f200582008479d
TudorOrban <tudororban2@gmail.com>
2025-02-14 15:23:58 +0200

    feat(k8): Establish connection between Tenant service and API Gateway

--- services/intnet-gateway/src/main/java/com/intnet/gateway/GatewayServiceApplication.java
+++ services/intnet-gateway/src/main/java/com/intnet/gateway/GatewayServiceApplication.java
@@ -2,18 +2,8 @@ package com.intnet.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		BatchAutoConfiguration.class,
-		JpaRepositoriesAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,})
-@EnableDiscoveryClient
+@SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- services/intnet-gateway/src/main/java/com/intnet/gateway/config/GatewayRouteConfig.java
+++ services/intnet-gateway/src/main/java/com/intnet/gateway/config/GatewayRouteConfig.java
@@ -1,4 +1,4 @@
-package com.intnet.gateway.core.config;
+package com.intnet.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
