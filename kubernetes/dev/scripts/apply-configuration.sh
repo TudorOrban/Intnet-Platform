90583b505a66eac0cab19c183121d1e08cc525d3
TudorOrban <tudororban2@gmail.com>
2025-02-15 20:27:06 +0200

    feat(network): Initialize Network microservice

--- kubernetes/dev/scripts/apply-configuration.sh
+++ kubernetes/dev/scripts/apply-configuration.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-cd ../config
+cd ../config || exit
 kubectl apply -f role.yaml
 kubectl apply -f role-binding.yaml
 
-cd ../test
+cd ../test || exit
 kubectl apply -f test-pod.yaml
 kubectl apply -f keycloak-persistent-volume.yaml
 kubectl apply -f keycloak.yaml
 
-cd ../gateway
+cd ../gateway || exit
 kubectl apply -f gateway-deployment.yaml
 kubectl apply -f gateway-service.yaml
 
-cd ../tenant
+cd ../tenant || exit
 kubectl apply -f tenant-deployment.yaml
 kubectl apply -f tenant-service.yaml
+
+cd ../network || exit
+kubectl apply -f network-deployment.yaml
+kubectl apply -f network-service.yaml
+
