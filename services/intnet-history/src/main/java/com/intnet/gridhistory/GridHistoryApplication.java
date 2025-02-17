b520e3503176780d5267f3dec0b1233c41ddb5f2
TudorOrban <tudororban2@gmail.com>
2025-02-17 22:36:05 +0200

    feat(history): Initialize Intnet Grid History microservice

---
+++ services/intnet-history/src/main/java/com/intnet/gridhistory/GridHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.intnet.gridhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridHistoryApplication.class, args);
+	}
+
+}
---
+++ services/intnet-history/src/test/java/com/intnet/gridhistory/GridHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intnet.gridhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
