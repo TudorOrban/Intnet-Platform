ef08fa29390e3971081030f00e4d576bea6c097d
TudorOrban <tudororban2@gmail.com>
2025-02-19 10:35:17 +0200

    feat(device-manager): Add Grid Device Manager microservice

---
+++ services/intnet-grid-device-manager/src/main/java/com/intnet/griddevicemanager/GridDeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.intnet.griddevicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridDeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridDeviceManagerApplication.class, args);
+	}
+
+}
---
+++ services/intnet-grid-device-manager/src/test/java/com/intnet/griddevicemanager/GridDeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intnet.griddevicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridDeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
