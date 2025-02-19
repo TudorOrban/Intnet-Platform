437bfe52350267ce66cddc5654bcfa1f9b7f5401
TudorOrban <tudororban2@gmail.com>
2025-02-19 19:13:05 +0200

    feat(adapter): Connect to modbus devices from Device Adapter

--- services/device-adapters/modbus-adapter/api/core/connection_handler.go
+++ services/device-adapters/modbus-adapter/api/core/connection_handler.go
@@ -1,16 +1,21 @@
 package core
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"modbusadapter/internal/device"
 	"modbusadapter/internal/kafka"
 	"net/http"
+	"time"
+
+	"github.com/goburrow/modbus"
 )
 
 type ConnectionHandler struct {
 	DeviceManager device.DeviceManager
-	Producer kafka.Producer
+	Producer      kafka.Producer
 }
 
 func NewConnectionHandler(
@@ -28,7 +33,39 @@ func (h *ConnectionHandler) handleStartConnectionHandler(w http.ResponseWriter, 
 	}
 	defer r.Body.Close()
 
-	log.Printf("Body: %s", body)
+	var devices []device.Device
+	if err := json.Unmarshal(body, &devices); err != nil {
+		http.Error(w, "Failed to unmarshal device list", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Received devices: %+v", devices)
+
+	for _, device := range devices {
+		go h.processDevice(device)
+	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func (h *ConnectionHandler) processDevice(device device.Device) {
+	_, handler, err := h.establishConnection(device) // Call separate connection function
+	if err != nil {
+		log.Printf("Error establishing connection to %s: %v", device.IPAddress, err)
+		return
+	}
+	defer handler.Close()
+
+}
+
+func (h *ConnectionHandler) establishConnection(device device.Device) (modbus.Client, *modbus.TCPClientHandler, error) {
+	ip := device.IPAddress
+	port := 502
+	address := fmt.Sprintf("%s:%d", ip, port)
+
+	handler := modbus.NewTCPClientHandler(address)
+	handler.Timeout = 10 * time.Second
+	client := modbus.NewClient(handler)
+
+	return client, handler, nil
 }
--- services/device-adapters/modbus-adapter/go.mod
+++ services/device-adapters/modbus-adapter/go.mod
@@ -6,3 +6,8 @@ require (
 	github.com/gorilla/mux v1.8.1
 	go.uber.org/dig v1.18.0
 )
+
+require (
+	github.com/goburrow/modbus v0.1.0 // indirect
+	github.com/goburrow/serial v0.1.0 // indirect
+)
