2cd1597f7ba57121ffd1c7bb758b9e7d496c8b61
TudorOrban <tudororban2@gmail.com>
2025-02-27 19:07:36 +0200

    fix(opf-gnn): Fix dependency issues with Pytorch Geometric

--- services/intnet-opf-gnn/src/main.py
+++ services/intnet-opf-gnn/src/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    print("intnet-opf-gnn service started!")
+    print("intnet-opf-gnn service!")
 
 if __name__ == "__main__":
     main()
