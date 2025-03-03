fix(opf-gnn): Fix normalization issues with new GNN features

services/intnet-ml/intnet-opf-gnn/src/example/training.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import mlflow # type: ignore
 import torch
 
-from core.models import SimpleGCN
+from experiment.models import SimpleGCN
 from torch_geometric.data import Data
 from torch.nn import MSELoss
 from torch.optim import Adam

services/intnet-ml/intnet-opf-gnn/src/experiment/data_pipeline.py
@@ -3,7 +3,7 @@ from typing import List
 import torch
 from torch_geometric.data import Data
 import numpy as np
-from core.synthetic_data import GridSample
+from experiment.synthetic_data import GridSample
 
 
 def prepare_data_for_gnn(samples: List[GridSample]) -> List[Data]:
@@ -16,9 +16,10 @@ def prepare_data_for_gnn(samples: List[GridSample]) -> List[Data]:
     load_power_values = np.array([sample.node_features[:, 4] for sample in samples]).flatten()
     gen_p_mw_values = np.array([sample.gen_p_mw for sample in samples])
     load_q_mvar_values = np.array([sample.node_features[:, 5] for sample in samples]).flatten()
-    gen_max_p_values = np.array([sample.node_features[:, 6] for sample in samples]).flatten()
-    gen_min_q_values = np.array([sample.node_features[:, 7] for sample in samples]).flatten()
-    gen_max_q_values = np.array([sample.node_features[:, 8] for sample in samples]).flatten()
+    gen_min_p_values = np.array([sample.node_features[:, 6] for sample in samples]).flatten()
+    gen_max_p_values = np.array([sample.node_features[:, 7] for sample in samples]).flatten()
+    gen_min_q_values = np.array([sample.node_features[:, 8] for sample in samples]).flatten()
+    gen_max_q_values = np.array([sample.node_features[:, 9] for sample in samples]).flatten()
 
     # Extract edge float features
     r_ohm_per_km_values = np.array([sample.edge_features[:, 0] for sample in samples]).flatten()
@@ -33,18 +34,22 @@ def prepare_data_for_gnn(samples: List[GridSample]) -> List[Data]:
     std_gen_p_mw = np.std(gen_p_mw_values)
     mean_load_q_mvar = np.mean(load_q_mvar_values)
     std_load_q_mvar = np.std(load_q_mvar_values)
+    mean_gen_min_p = np.mean(gen_min_p_values)
+    std_gen_min_p = np.std(gen_min_p_values)
     mean_gen_max_p = np.mean(gen_max_p_values)
     std_gen_max_p = np.std(gen_max_p_values)
     mean_gen_min_q = np.mean(gen_min_q_values)
     std_gen_min_q = np.std(gen_min_q_values)
     mean_gen_max_q = np.mean(gen_max_q_values)
     std_gen_max_q = np.std(gen_max_q_values)
-
+    print("Max min", mean_load_q_mvar, std_load_q_mvar, mean_gen_min_q, mean_gen_max_q)
     # Calculate normalization parameters for edge features
     mean_r_ohm_per_km = np.mean(r_ohm_per_km_values)
     std_r_ohm_per_km = np.std(r_ohm_per_km_values)
     mean_x_ohm_per_km = np.mean(x_ohm_per_km_values)
     std_x_ohm_per_km = np.std(x_ohm_per_km_values)
+
+    print("R ohm", mean_r_ohm_per_km, std_r_ohm_per_km, mean_x_ohm_per_km, std_x_ohm_per_km)
 
     epsilon = 1e-7
 
@@ -56,23 +61,28 @@ def prepare_data_for_gnn(samples: List[GridSample]) -> List[Data]:
         normalized_vm_pu = (sample.node_features[:, 3] - mean_vm_pu) / (std_vm_pu if std_vm_pu != 0 else epsilon)
         normalized_load_power = (sample.node_features[:, 4] - mean_load_power) / (std_load_power if std_load_power != 0 else epsilon)
         normalized_load_q_mvar = (sample.node_features[:, 5] - mean_load_q_mvar) / (std_load_q_mvar if std_load_q_mvar != 0 else epsilon)
-        normalized_gen_max_p = (sample.node_features[:, 6] - mean_gen_max_p) / (std_gen_max_p if std_gen_max_p != 0 else epsilon)
-        normalized_gen_min_q = (sample.node_features[:, 7] - mean_gen_min_q) / (std_gen_min_q if std_gen_min_q != 0 else epsilon)
-        normalized_gen_max_q = (sample.node_features[:, 8] - mean_gen_max_q) / (std_gen_max_q if std_gen_max_q != 0 else epsilon)
+        normalized_gen_min_p = (sample.node_features[:, 6] - mean_gen_min_p) / (std_gen_min_p if std_gen_min_p != 0 else epsilon)
+        normalized_gen_max_p = (sample.node_features[:, 7] - mean_gen_max_p) / (std_gen_max_p if std_gen_max_p != 0 else epsilon)
+        normalized_gen_min_q = (sample.node_features[:, 8] - mean_gen_min_q) / (std_gen_min_q if std_gen_min_q != 0 else epsilon)
+        normalized_gen_max_q = (sample.node_features[:, 9] - mean_gen_max_q) / (std_gen_max_q if std_gen_max_q != 0 else epsilon)
 
         normalized_features = np.copy(sample.node_features)
         normalized_features[:, 3] = normalized_vm_pu
         normalized_features[:, 4] = normalized_load_power
         normalized_features[:, 5] = normalized_load_q_mvar
-        normalized_features[:, 6] = normalized_gen_max_p
-        normalized_features[:, 7] = normalized_gen_min_q
-        normalized_features[:, 8] = normalized_gen_max_q
+        normalized_features[:, 6] = normalized_gen_min_p
+        normalized_features[:, 7] = normalized_gen_max_p
+        normalized_features[:, 8] = normalized_gen_min_q
+        normalized_features[:, 9] = normalized_gen_max_q
 
         x = torch.tensor(normalized_features, dtype=torch.float)
         y = torch.tensor([(sample.gen_p_mw - mean_gen_p_mw) / (std_gen_p_mw if std_gen_p_mw != 0 else epsilon)], dtype=torch.float)
 
         normalized_r_ohm_per_km = (sample.edge_features[:, 0] - mean_r_ohm_per_km) / (std_r_ohm_per_km if std_r_ohm_per_km != 0 else epsilon)
-        normalized_x_ohm_per_km = (sample.edge_features[:, 1] - mean_x_ohm_per_km) / (std_x_ohm_per_km if std_x_ohm_per_km != 0 else epsilon)
+        if std_x_ohm_per_km != 0:
+            normalized_x_ohm_per_km = (sample.edge_features[:, 1] - mean_x_ohm_per_km) / (std_x_ohm_per_km if std_x_ohm_per_km != 0 else epsilon)
+        else:
+            normalized_x_ohm_per_km = np.zeros_like(sample.edge_features[:, 1])
 
         normalized_edge_features = np.stack((normalized_r_ohm_per_km, normalized_x_ohm_per_km), axis=1)
         edge_attr = torch.tensor(normalized_edge_features, dtype=torch.float)

services/intnet-ml/intnet-opf-gnn/src/experiment/model.py
@@ -28,7 +28,7 @@ class SimpleGCN(torch.nn.Module):
         x = torch.mean(x, dim=0)
         return x
     
-def train_gnn(data_list: List[Data], epochs=100, hidden_channels=64, lr=0.01, patience=10, weight_decay=1e-5):
+def train_gnn(data_list: List[Data], epochs=100, hidden_channels=64, lr=0.01, patience=10, weight_decay=1e-5, dropout_rate=0.5):
     """Trains the GNN model."""
 
     train_test_ratio = 0.8
@@ -36,7 +36,7 @@ def train_gnn(data_list: List[Data], epochs=100, hidden_channels=64, lr=0.01, pa
     train_data = data_list[:train_size]
     val_data = data_list[train_size:]
 
-    model = SimpleGCN(num_node_features=train_data[0].x.shape[1], hidden_channels=hidden_channels)
+    model = SimpleGCN(num_node_features=train_data[0].x.shape[1], hidden_channels=hidden_channels, dropout_rate=dropout_rate)
     optimizer = torch.optim.Adam(model.parameters(), lr, weight_decay=weight_decay)
     criterion = torch.nn.MSELoss()
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min", patience=patience // 2, factor=0.5, verbose=True)

services/intnet-ml/intnet-opf-gnn/src/experiment/synthetic_data.py
@@ -53,16 +53,18 @@ def generate_synthetic_data(num_samples=10) -> List[GridSample]:
                 load_power = load_p_mw[load_index]
                 load_q_mvar = net.load.at[load_index, "q_mvar"]
 
+            gen_min_p = 0.0
             gen_max_p = 0.0
             gen_min_q = 0.0
             gen_max_q = 0.0
             if gen_present == 1.0:
                 gen_index = net.gen.bus.values.tolist().index(bus_idx)
+                gen_min_p = net.gen.at[net.gen.bus.values[gen_index], "min_p_mw"]
                 gen_max_p = net.gen.at[net.gen.bus.values[gen_index], "max_p_mw"]
                 gen_min_q = net.gen.at[net.gen.bus.values[gen_index], "min_q_mvar"]
                 gen_max_q = net.gen.at[net.gen.bus.values[gen_index], "max_q_mvar"]
 
-            node_features.append([bus_type, load_present, gen_present, vm_pu, load_power, load_q_mvar, gen_max_p, gen_min_q, gen_max_q])
+            node_features.append([bus_type, load_present, gen_present, vm_pu, load_power, load_q_mvar, gen_min_p, gen_max_p, gen_min_q, gen_max_q])
 
         node_features = np.array(node_features)
 
@@ -98,7 +100,7 @@ def generate_random_network(p_mw_load_1: float, p_mw_load_2: float, p_mw_load_3:
 
     # Generators
     # (Slack)
-    pp.create_gen(net, bus=bus1, p_mw=50., min_p_mw=0., max_p_mw=150., q_mvar=0., max_q_mvar=150., slack=True)
+    pp.create_gen(net, bus=bus1, p_mw=50., min_p_mw=0., max_p_mw=150., q_mvar=0., min_q_mvar=0., max_q_mvar=150., slack=True)
     pp.create_poly_cost(net, element=0, et="gen", cp1_eur_per_mw=10.) 
     
     # Loads