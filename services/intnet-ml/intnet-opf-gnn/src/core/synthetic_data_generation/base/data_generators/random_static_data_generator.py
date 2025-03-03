e7c8c9a1481e507d6997b6c4a40df7139045dc4f
TudorOrban <tudororban2@gmail.com>
2025-03-03 15:54:36 +0200

    feat(opf-gnn): Add generator of synthetic data for static grid properties

---
+++ services/intnet-ml/intnet-opf-gnn/src/core/synthetic_data_generation/base/data_generators/random_static_data_generator.py
@@ -0,0 +1,73 @@
+
+import random
+from core.data_types import EdgeType, GridGraphData
+
+
+def generate_random_static_data(graph_data: GridGraphData) -> GridGraphData:
+    """Generates realistic random static electric data for the electric grid components"""
+
+    graph_data = generate_random_load_data(graph_data)
+    graph_data = generate_random_generator_data(graph_data)
+    graph_data = generate_random_bus_data(graph_data)
+    graph_data = generate_random_edge_data(graph_data)
+
+def generate_random_load_data(graph_data: GridGraphData) -> GridGraphData:
+    for bus in graph_data.buses:
+        for load in bus.loads:
+            load.min_p_mw = 0
+            load.max_p_mw = random.uniform(10, 200)
+            load.min_q_mvar = -random.uniform(5, 100)
+            load.max_q_mvar = random.uniform(5, 100)
+
+    return graph_data
+
+def generate_random_generator_data(graph_data: GridGraphData) -> GridGraphData:
+    available_buses = [bus for bus in graph_data.buses if bus.generators]
+    if not available_buses:
+        return graph_data
+    
+    total_generators = sum(len(bus.generators) for bus in available_buses)
+    if total_generators <= 0:
+        return graph_data
+    
+    total_load_max_p_mw = sum(load.max_p_mw for bus in graph_data.buses for load in bus.loads)
+    total_load_min_q_mvar = sum(load.min_q_mvar for bus in graph_data.buses for load in bus.loads)
+    total_load_max_q_mvar = sum(load.max_q_mvar for bus in graph_data.buses for load in bus.loads)
+
+    safety_margin = 1.2
+    variation = 0.2
+
+    max_p_mw_per_gen = determine_generator_specification(total_load_max_p_mw, safety_margin, total_generators)
+    min_q_mvar_per_gen = determine_generator_specification(total_load_min_q_mvar, safety_margin, total_generators)
+    max_q_mvar_per_gen = determine_generator_specification(total_load_max_q_mvar, safety_margin, total_generators)
+    
+    for bus in available_buses:
+        for gen in bus.generators:
+            gen.min_p_mw = 0
+            gen.max_p_mw = random.uniform(max_p_mw_per_gen * (1 - variation), max_p_mw_per_gen * (1 + variation))
+            gen.min_q_mvar = random.uniform(min_q_mvar_per_gen * (1 - variation), min_q_mvar_per_gen * (1 + variation))
+            gen.max_q_mvar = random.uniform(max_q_mvar_per_gen * (1 - variation), max_q_mvar_per_gen * (1 + variation))
+
+def determine_generator_specification(specification: float, safety_margin: float, total_generators: int) -> float:
+    return specification * safety_margin / total_generators
+
+def generate_random_bus_data(graph_data: GridGraphData) -> GridGraphData:
+    for bus in graph_data.buses:
+        bus.vn_kv = random.choice([110, 220, 400])
+        bus.min_vm_pu = 0.95
+        bus.max_vm_pu = 1.05
+
+def generate_random_edge_data(graph_data: GridGraphData) -> GridGraphData:
+    for edge in graph_data.edges:
+        if edge.edge_type == EdgeType.TRANSMISSION_LINE:
+            edge.r_ohm_per_km = random.uniform(0.01, 0.1)
+            edge.x_ohm_per_km = random.uniform(0.1, 0.5)
+            edge.length_km = random.uniform(10, 200)
+        elif edge.edge_type == EdgeType.DISTRIBUTION_LINE:
+            edge.r_ohm_per_km = random.uniform(0.1, 1.0)
+            edge.x_ohm_per_km = random.uniform(0.05, 0.2)
+            edge.length_km = random.uniform(1, 20)
+        elif edge.edge_type == EdgeType.TRANSFORMER:
+            edge.r_ohm_per_km = random.uniform(0.001, 0.01)
+            edge.x_ohm_per_km = random.uniform(0.01, 0.05)
+            edge.length_km = 0
