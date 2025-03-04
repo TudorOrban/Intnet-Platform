40e4e462eaaabe99b7365acae2c90a319f988437
TudorOrban <tudororban2@gmail.com>
2025-03-04 20:27:55 +0200

    feat(opf-gnn): Add weight decay, dropout and early stopping

--- services/intnet-ml/intnet-opf-gnn/src/core/synthetic_data_generation/base/model/base_gnn.py
+++ services/intnet-ml/intnet-opf-gnn/src/core/synthetic_data_generation/base/model/base_gnn.py
@@ -10,21 +10,22 @@ class BaseGNN(torch.nn.Module):
         super(BaseGNN, self).__init__()
         self.conv1 = GCNConv(num_node_features, hidden_channels)
         self.conv2 = GCNConv(hidden_channels, 1)
+        self.dropout_rate = dropout_rate
         
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
+        x = F.dropout(x, p=self.dropout_rate, training=self.training)
         x = self.conv2(x, edge_index)
 
         generator_mask = (data.x[:, 0] == 1)
-
         generator_outputs = x[generator_mask].squeeze(1)
         
         return generator_outputs
     
-def train_gnn(input_data: List[Data], epochs=100, hidden_channels=64, lr=0.01):
+def train_gnn(input_data: List[Data], epochs=100, hidden_channels=64, lr=0.01, weight_decay=1e-5, dropout_rate=0.4, patience=10):
     """Trains the GNN model."""
 
     train_test_ratio = 0.8
@@ -32,10 +33,14 @@ def train_gnn(input_data: List[Data], epochs=100, hidden_channels=64, lr=0.01):
     train_data = input_data[:train_size]
     val_data = input_data[train_size:]
 
-    model = BaseGNN(num_node_features=train_data[0].x.shape[1], hidden_channels=hidden_channels)
-    optimizer = torch.optim.Adam(model.parameters(), lr)
+    model = BaseGNN(num_node_features=train_data[0].x.shape[1], hidden_channels=hidden_channels, dropout_rate=dropout_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr, weight_decay=weight_decay)
     criterion = torch.nn.MSELoss()
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min", patience=patience // 2, factor=0.5, verbose=True)
     
+    best_val_loss = float("inf")
+    epochs_no_improve = 0
+
     model.train()
     for epoch in range(epochs):
         total_train_loss = 0
@@ -57,10 +62,23 @@ def train_gnn(input_data: List[Data], epochs=100, hidden_channels=64, lr=0.01):
 
         avg_train_loss = total_train_loss / len(train_data)
         avg_val_loss = total_val_loss / len(val_data)
+        scheduler.step(avg_val_loss)
 
         print(f"Epoch {epoch + 1}, Train Loss: {avg_train_loss}, Val Loss: {avg_val_loss}")
         
         mlflow.log_metric("train_loss", avg_train_loss, step=epoch)
         mlflow.log_metric("val_loss", avg_val_loss, step=epoch)
 
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            epochs_no_improve = 0
+            torch.save(model.state_dict(), "best_model.pth")
+        else:
+            epochs_no_improve += 1
+            if epochs_no_improve == patience:
+                print("Early stopping!")
+                break
+        model.train()
+
+    model.load_state_dict(torch.load("best_model.pth"))
     return model
--- services/intnet-ml/intnet-opf-gnn/src/core/synthetic_data_generation/base/model/mlflow_run.py
+++ services/intnet-ml/intnet-opf-gnn/src/core/synthetic_data_generation/base/model/mlflow_run.py
@@ -10,18 +10,24 @@ def run_mlflow_train_gnn():
         epochs = 100
         hidden_channels = 256
         lr = 0.01
+        weight_decay=1e-5
+        dropout_rate=0
+        patience = 10
 
         mlflow.log_param("samples_count", samples_count)
         mlflow.log_param("epochs", epochs)
         mlflow.log_param("hidden_channels", hidden_channels)
         mlflow.log_param("lr", lr)
+        mlflow.log_param("weight_decay", weight_decay)
+        mlflow.log_param("dropout_rate", dropout_rate)
+        mlflow.log_param("patience", patience)
 
         flatSampleRepository = FlatTrainingSampleRepository()
         samples = flatSampleRepository.read_samples(limit=samples_count)
         
         input_data = map_flat_samples_to_pytorch_data(samples)
         
-        trained_model = train_gnn(input_data=input_data, epochs=epochs, hidden_channels=hidden_channels, lr=lr)
+        trained_model = train_gnn(input_data=input_data, epochs=epochs, hidden_channels=hidden_channels, lr=lr, weight_decay=weight_decay, dropout_rate=dropout_rate, patience=patience)
         
         mlflow.pytorch.log_model(trained_model, "model")
         mlflow.set_tags({"model_type": "GCN", "dataset": "synthetic"})
