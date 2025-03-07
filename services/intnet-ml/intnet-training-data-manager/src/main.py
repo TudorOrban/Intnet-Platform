d5fb05e9ed7b38afe9c51a89576dfcb185583d5b
TudorOrban <tudororban2@gmail.com>
2025-03-07 18:11:41 +0200

    feat(opf-gnn): Add MongoDB Helm chart

--- services/intnet-ml/intnet-training-data-manager/src/main.py
+++ services/intnet-ml/intnet-training-data-manager/src/main.py
@@ -1,9 +1,6 @@
 import logging
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from dotenv import load_dotenv
-from cassandra.cluster import Session
-
-from core.storage.cassandra import get_cassandra_session
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,12 +12,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Intnet Training Data Manager Service is running"}
-
-
-@app.get("/cassandra")
-async def cassandra(session: Session = Depends(get_cassandra_session)):
-    try:
-        result = session.execute("SELECT now() FROM system.local")
-        return {"cassandra_time": str(result[0].now)}
-    except Exception as e:
-        return {"error": str(e)}
