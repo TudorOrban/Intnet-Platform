d07da12240101213f5d133e85d94cadf4427839d
TudorOrban <tudororban2@gmail.com>
2025-03-10 15:30:09 +0200

    fix(admin): Fix issues with Rollout Restart Deployment functionality

--- services/intnet-admin/src/main/java/com/intnet/admin/features/intnetservice/service/IntnetServiceKubernetesServiceImpl.java
+++ services/intnet-admin/src/main/java/com/intnet/admin/features/intnetservice/service/IntnetServiceKubernetesServiceImpl.java
@@ -5,6 +5,8 @@ import com.intnet.admin.features.intnetservice.model.ServiceStatus;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.apis.AppsV1Api;
 import io.kubernetes.client.openapi.models.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -16,6 +18,7 @@ import java.util.Map;
 public class IntnetServiceKubernetesServiceImpl implements IntnetServiceKubernetesService {
 
     private final AppsV1Api appsV1Api;
+    private final Logger logger = LoggerFactory.getLogger(IntnetServiceKubernetesServiceImpl.class);
 
     public IntnetServiceKubernetesServiceImpl(AppsV1Api appsV1Api) {
         this.appsV1Api = appsV1Api;
@@ -36,7 +39,7 @@ public class IntnetServiceKubernetesServiceImpl implements IntnetServiceKubernet
                 ServiceKubernetesData data = this.getKubernetesData(status);
                 serviceDataMap.put(serviceName, data);
             } catch (ApiException e) {
-                System.err.println("Error fetching Kubernetes data for " + serviceName + ": " + e.getResponseBody());
+                logger.error("Error fetching Kubernetes data for {}: {}", serviceName, e.getResponseBody());
                 serviceDataMap.put(serviceName, null);
             }
         }
@@ -50,23 +53,33 @@ public class IntnetServiceKubernetesServiceImpl implements IntnetServiceKubernet
         for (String serviceName : serviceNames) {
             try {
                 V1Deployment deployment = appsV1Api.readNamespacedDeployment(serviceName, namespace).execute();
-                if (deployment.getMetadata() == null) {
-                    System.out.println("Failed to get service metadata for: " + serviceName);
+
+                if (deployment.getMetadata() == null || deployment.getSpec() == null) {
+                    logger.warn("Failed to get service metadata or template for: {}", serviceName);
+                    continue;
+                } else {
+                    deployment.getSpec().getTemplate();
+                }
+
+                V1PodTemplateSpec template = deployment.getSpec().getTemplate();
+                if (template.getMetadata() == null) {
+                    logger.warn("Failed to get template metadata: {}", serviceName);
                     continue;
                 }
-                // Create a new annotation for the restart
-                Map<String, String> annotations = deployment.getMetadata().getAnnotations();
+                Map<String, String> annotations = template.getMetadata().getAnnotations();
+
                 if (annotations == null) {
                     annotations = new HashMap<>();
                 }
+
                 annotations.put("kubectl.kubernetes.io/restartedAt", Instant.now().toString());
 
-                deployment.getMetadata().setAnnotations(annotations);
-                appsV1Api.replaceNamespacedDeployment(serviceName, namespace, deployment);
+                template.getMetadata().setAnnotations(annotations);
+                appsV1Api.replaceNamespacedDeployment(serviceName, namespace, deployment).execute();
 
-                System.out.println("Rollout restart initiated for deployment: " + serviceName + " in namespace: " + namespace);
+                logger.info("Rollout restart initiated for deployment: {} in namespace: {}", serviceName, namespace);
             } catch (ApiException e) {
-                System.err.println("Error restarting deployment: " + serviceName + ". Error: " + e);
+                logger.error("Error restarting deployment: {}. Error: {}", serviceName, e);
             }
         }
     }
