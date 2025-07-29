826d8863807af8627e860bdf58885bb1b698b86f
Tudor Andrei Orban <130213626+TudorOrban@users.noreply.github.com>
2025-07-29 17:33:26 +0300

    Update BusStateRecordServiceImpl.java

--- services/intnet-history/src/main/java/com/intnet/gridhistory/features/bus/service/BusStateRecordServiceImpl.java
+++ services/intnet-history/src/main/java/com/intnet/gridhistory/features/bus/service/BusStateRecordServiceImpl.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/*
+ * Service responsible for CRUD ops for Bus State Record entity
+ */
 @Service
 public class BusStateRecordServiceImpl implements BusStateRecordService {
 
