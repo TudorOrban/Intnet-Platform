a3588034c4806435569ae817b308f067014f4194
Tudor Andrei Orban <130213626+TudorOrban@users.noreply.github.com>
2025-07-31 18:37:20 +0300

    Update DeviceServiceImpl.java

--- services/intnet-grid-device-manager/src/main/java/com/intnet/griddevicemanager/features/device/service/DeviceServiceImpl.java
+++ services/intnet-grid-device-manager/src/main/java/com/intnet/griddevicemanager/features/device/service/DeviceServiceImpl.java
@@ -15,6 +15,9 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/*
+ * Service responsible for managing grid devices
+ */
 @Service
 public class DeviceServiceImpl implements DeviceService {
 
