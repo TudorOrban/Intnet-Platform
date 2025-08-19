b54182e2c727e35873fe794d9f9cc5a537de66a9
Tudor Andrei Orban <130213626+TudorOrban@users.noreply.github.com>
2025-08-19 18:10:05 +0300

    Update LoadServiceImpl.java

--- services/intnet-grid-data/src/main/java/com/intnet/griddata/features/load/service/LoadServiceImpl.java
+++ services/intnet-grid-data/src/main/java/com/intnet/griddata/features/load/service/LoadServiceImpl.java
@@ -16,6 +16,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/*
+ * Service responsible for CRUD ops for the Load entity
+ */
 @Service
 public class LoadServiceImpl implements LoadService {
 
