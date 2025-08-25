c22c7cd9b9979cc93bf2ceea1f05c7ff86c2d132
Tudor Andrei Orban <130213626+TudorOrban@users.noreply.github.com>
2025-08-25 15:02:05 +0300

    Update DeviceController.java

--- services/intnet-grid-device-manager/src/main/java/com/intnet/griddevicemanager/features/device/controller/DeviceController.java
+++ services/intnet-grid-device-manager/src/main/java/com/intnet/griddevicemanager/features/device/controller/DeviceController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/*
+ * Controller for the Device feature
+ */
 @RestController
 @RequestMapping("api/v1/devices")
 public class DeviceController {
