08051128bdd9d49e93a765c6ed1c1dcb76153c33
Tudor Andrei Orban <130213626+TudorOrban@users.noreply.github.com>
2025-10-07 22:12:30 +0300

    Update GeneratorServiceImpl.java

--- services/intnet-grid-data/src/main/java/com/intnet/griddata/features/generator/service/GeneratorServiceImpl.java
+++ services/intnet-grid-data/src/main/java/com/intnet/griddata/features/generator/service/GeneratorServiceImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 /*
- * Service responsible for CRUD ops for Generator entity
+ * Service responsible for CRUD ops for Generator feature
  */
 @Service
 public class GeneratorServiceImpl implements GeneratorService {
